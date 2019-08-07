1ecd40752e246d5097abdfcb32b04a653cac7c37
Markus John <johm@quyo.de>
2019-08-07 16:07:55 +0000

    Add /time

--- src/Klio.jl
+++ src/Klio.jl
@@ -1,5 +1,19 @@
 module Klio
 
-greet() = print("Hello World!")
+using Dates
+using Genie
+
+import Genie.Router: route, POST
+import Genie.Renderer: json
+
+run() = begin
+    Genie.config.run_as_server = true
+
+    route("/time", method = POST) do
+        Dict(:response_type => "in_channel", :text => Dates.now(Dates.UTC)) |> json
+    end
+
+    Genie.startup()
+end
 
 end # module
