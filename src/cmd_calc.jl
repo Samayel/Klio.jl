b6b5b68c815aedcc12d765e2d16ad91bb565c177
Markus John <johm@quyo.de>
2019-09-11 14:20:19 +0000

    Add Maxima(.jl) to /calc

--- src/cmd_calc.jl
+++ src/cmd_calc.jl
@@ -1,5 +1,6 @@
 module Calc
 
+using Maxima
 using Reduce
 
 using ..Mattermost
@@ -7,19 +8,39 @@ using ..Mattermost
 reduce_initialized = false
 
 function calc(req)
+    startswith(req.text, "!calc --maxima ") && return mcalc(replace(req.text, "!calc --maxima " => ""))
+    startswith(req.text, "!calc --reduce ") && return rcalc(replace(req.text, "!calc --reduce " => ""))
+    mcalc(replace(req.text, "!calc " => ""))
+end
+
+function rcalc(question)
     global reduce_initialized
     if !reduce_initialized
         rcall("load_package RESET")
         rcall("load_package RLFI")
-        rcall("1+1")
         reduce_initialized = true
     end
-    question = replace(req.text, "!calc " => "")
     try rcall("RESETREDUCE") catch; end
-    answer = rcall(question, :latex) |> string |> chomp
+    answer = rcall(question, :latex)
     answer = replace(answer, "\\begin{displaymath}" => "")
     answer = replace(answer, "\\end{displaymath}" => "")
-    return OutgoingWebhookResponse("```latex\n" * answer * "\n```")
+    answer = strip(answer)
+    if occursin('^', answer) || occursin('_', answer) || occursin('\\', answer) || occursin('\n', answer)
+        return OutgoingWebhookResponse("```latex\n" * answer * "\n```")
+    else
+        return OutgoingWebhookResponse("`" * answer * "`")
+    end
+end
+
+function mcalc(question)
+    try
+        mcall("kill (all)")
+        mcall("reset ()")
+        mcall("display2d: false")
+    catch
+    end
+    answer = mcall(question)
+    return OutgoingWebhookResponse("`" * answer * "`")
 end
 
 end
