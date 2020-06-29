ec985702d122a18413ab93e49efae94fe5884a91
Markus John <johm@quyo.de>
2020-06-29 18:14:37 +0000

    Switch to Mathematica v12.1

--- run.jl
+++ run.jl
@@ -2,8 +2,8 @@
 
 using Klio
 
-ENV["JULIA_MATHKERNEL"] = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.0/Executables")
-ENV["JULIA_MATHLINK"]   = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.0/SystemFiles/Libraries/Linux-x86-64")
+ENV["JULIA_MATHKERNEL"] = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.1/Executables")
+ENV["JULIA_MATHLINK"]   = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.1/SystemFiles/Libraries/Linux-x86-64")
 
 Klio.settings.expl_sqlite_file = joinpath(@__DIR__, "db/expl.sqlite")
 
