3707d9d33463b026112bf64848c6cc2b4fb85ba8
Markus John <johm@quyo.de>
2020-06-29 18:30:34 +0000

    Fix MathLink initialization

--- run.jl
+++ run.jl
@@ -2,8 +2,8 @@
 
 using Klio
 
-ENV["JULIA_MATHKERNEL"] = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.1/Executables")
-ENV["JULIA_MATHLINK"]   = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.1/SystemFiles/Libraries/Linux-x86-64")
+ENV["JULIA_MATHKERNEL"] = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.1/Executables/WolframKernel")
+ENV["JULIA_MATHLINK"]   = joinpath(ENV["HOME"], "opt/Wolfram/Mathematica/12.1/SystemFiles/Libraries/Linux-x86-64/libML$(Sys.WORD_SIZE)i4")
 
 Klio.settings.expl_sqlite_file = joinpath(@__DIR__, "db/expl.sqlite")
 
